e03a8cec10333953cffa738adc246393a40d0010
HermanoCastro65 <hermanocastro18@gmail.com>
2022-10-19 18:19:42 -0300

    change main activity to show calendar view

--- app/src/main/java/com/example/customcalendarviewwithevents/MainActivity.java
+++ app/src/main/java/com/example/customcalendarviewwithevents/MainActivity.java
@@ -6,9 +6,12 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    CustumCalendarView customCalendarView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        customCalendarView = (CustumCalendarView)findViewById(R.id.custom_calendar_view);
     }
 }
