775159f8d3f0bb37fd27f219ff02f4ab297dc7e8
HermanoCastro65 <hermanocastro18@gmail.com>
2022-10-19 17:01:27 -0300

    create db open helper

---
+++ app/src/main/java/com/example/customcalendarviewwithevents/DBOpenHelper.java
@@ -0,0 +1,23 @@
+package com.example.customcalendarviewwithevents;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import androidx.annotation.Nullable;
+
+public class DBOpenHelper extends SQLiteOpenHelper {
+    public DBOpenHelper(@Nullable Context context) {
+        super(context);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase sqLiteDatabase) {
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
+
+    }
+}
